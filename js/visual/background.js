dcc0ae5f400885d7f35e922277c7d58c28bfae15
Incept <incept2ed@gmail.com>
2017-01-05 16:44:03 +1100

    Random image from imgur > /r/earthporn

--- js/visual/background.js
+++ js/visual/background.js
@@ -1,19 +1,31 @@
 let Background = new function() {
     
-    let urls = [
-    "u9muu7r", "elUmrNS", "TcA4IsQ", "PaMnxZn", "P7hwlaN", 
-    "I5O4QWi", "fT4bxpb", "U7Bx7FQ", "Qujelxk", "KAHqXM2", 
-    "laGeYSO", "HdsWnkU", "xEanEAB", "NG3moRJ", "31E8sfB",  
-    "XGiYXHs", "QBAbrBJ", "uclwgUc", "koPzyZ1", "8VfPY96"  
-    ];
-
     this.setUp = function() {
-        let index = urls[Math.floor(Math.random() * urls.length)];
+
+        $.ajax({
+            url: 'https://api.imgur.com/3/gallery/r/earthporn/0',
+            method: 'GET',
+            headers: {
+                Authorization: 'Client-ID 0428dcb72fbc5da',
+                Accept: 'application/json'
+            },
+            data: {
+                image: localStorage.dataBase64,
+                type: 'base64'
+            },
+            success: handleData
+        });
+ 
+    }
+    
+    let handleData = function(result) {
+        let index = result.data[Math.floor(Math.random() * result.data.length)].id;
         document.getElementById("bgimg1").src = "https://i.imgur.com/" + index +".jpg";
         document.getElementById("bgimg2").src = "https://i.imgur.com/" + index +".jpg";
         document.getElementById("limg1").src = "https://i.imgur.com/" + index +"m.jpg";
         document.getElementById("limg2").src = "https://i.imgur.com/" + index +"m.jpg";
     }
+    
 }
 
 $(window).on("load", function() {
