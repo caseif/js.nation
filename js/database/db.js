f680fe47a468084508c35e4b92eed398038dac0f
Max Roncace <me@caseif.net>
2017-01-05 19:31:31 -0500

    No more vars

--- js/database/db.js
+++ js/database/db.js
@@ -1,14 +1,14 @@
 //TODO: some serious organization/structure improvement
-//TODO: replace var with let
+//TODO: replace let with let
 
-var lastTouch = 0;
+let lastTouch = 0;
 const SLIDE_DOWN_TIME = 1000;
 
 // Listen Elements
-var elmFile = document.getElementById('fileSelector');
-var elmAdd = document.getElementById('add2DB');
-var elmView = document.getElementById('viewDB');
-var elmDeldb = document.getElementById('delDB');
+let elmFile = document.getElementById('fileSelector');
+let elmAdd = document.getElementById('add2DB');
+let elmView = document.getElementById('viewDB');
+let elmDeldb = document.getElementById('delDB');
 // Add Listeners
 elmFile.addEventListener('change', handleFileSelection, false);
 elmAdd.addEventListener('click', handleAdd, false);
@@ -16,17 +16,17 @@ elmView.addEventListener('click', handleView, false);
 elmDeldb.addEventListener('click', handleDeleteDB, false);
 
 // Global Stores
-var fileStore;
-var imgStore;
+let fileStore;
+let imgStore;
 
 // Misc Elements
-var elmTitle = document.getElementById('title');
-var elmArtist = document.getElementById('artist');
-var elmImage0 = document.getElementById('img');
-var elmImage1 = document.getElementById('bgimg1');
-var elmImage2 = document.getElementById('bgimg2');
-var elmAudio = document.getElementById('audio');
-var elmTable = document.getElementById('display');
+let elmTitle = document.getElementById('title');
+let elmArtist = document.getElementById('artist');
+let elmImage0 = document.getElementById('img');
+let elmImage1 = document.getElementById('bgimg1');
+let elmImage2 = document.getElementById('bgimg2');
+let elmAudio = document.getElementById('audio');
+let elmTable = document.getElementById('display');
 
 let Db = new function() {
     let timeoutId;
@@ -43,20 +43,20 @@ let Db = new function() {
 }
 
 // Debug Element | xalert('message');
-var elmMessages = document.getElementById('messages');
+let elmMessages = document.getElementById('messages');
 function xalert(message) {
     elmMessages.innerHTML += message + "<br>";
 }
 
 // Delete Database
-function handleDeleteDB(){
+function handleDeleteDB() {
     db.delete();
     handleView();
     xalert("You'll need to refresh");
 }
 
 // Create Database
-var db = new Dexie("visDB");
+let db = new Dexie("visDB");
 db.version(1).stores({id3: "++id, artist, title, duration, img, audio"});
 db.open().catch(e => xalert("Open failed: " + e));
 
@@ -65,18 +65,19 @@ function handleFileSelection(e) {
     //reset globals
     imgStore = undefined;
     fileStore = e.target.files[0];
-    
+
+    let url;
     try {
-        var url = URL.createObjectURL(fileStore);
+        url = URL.createObjectURL(fileStore);
     } catch (error) {
         return;
     }
     ID3.loadTags(url, () => {
-        var tags = ID3.getAllTags(url);
+        let tags = ID3.getAllTags(url);
         if (tags.picture !== undefined) {
 			// Convert picture to base64
-            var image = tags.picture; var base64String = "";
-            for (var i = 0; i < image.data.length; i++) {
+            let image = tags.picture; let base64String = "";
+            for (let i = 0; i < image.data.length; i++) {
                 base64String += String.fromCharCode(image.data[i]);
             }
             imgurl = "data:" + image.format + ";base64," + window.btoa(base64String);
@@ -104,10 +105,10 @@ function handleFileSelection(e) {
 }
 
 function handleAdd() {
-    var image = imgStore;
-    var artist = elmArtist.value;
-    var title = elmTitle.value;
-    var duration = secondsToHms(elmAudio.duration);
+    let image = imgStore;
+    let artist = elmArtist.value;
+    let title = elmTitle.value;
+    let duration = secondsToHms(elmAudio.duration);
     db.id3.add({artist: artist, title: title, duration: duration, img: image, audio: fileStore});
     handleView();
 }
@@ -116,26 +117,26 @@ function handleView() {
     elmTable.innerHTML = "";
     
     db.id3.each(result => {
-        var tr = "<tr>";
-        if (result.img !== undefined){
-            var td1 = "<td><img width='50px' src=\"" + result.img + "\"></td>";
-        }else{
-            var td1 = "<td></td>";
+        let tr = "<tr>";
+        let td1;
+        if (result.img !== undefined) {
+            td1 = "<td><img width='50px' src=\"" + result.img + "\"></td>";
+        } else {
+            td1 = "<td></td>";
         }
-        var td2 = "<td><a onclick=\"javascript:handlePlay(" + result.id + ")\">Play</a></td>";
-        var td3 = "<td>" + result.title + "</td>";
-        var td4 = "<td>" + result.artist + "</td>";
-        var td5 = "<td><a onclick=\"javascript:handleRemove(" + result.id + ")\">Remove</a></td>";
-        var td6 = "<td>" + result.duration + "</td>";
-        var tr2 = "</tr></td>";
+        let td2 = "<td><a onclick=\"javascript:handlePlay(" + result.id + ")\">Play</a></td>";
+        let td3 = "<td>" + result.title + "</td>";
+        let td4 = "<td>" + result.artist + "</td>";
+        let td5 = "<td><a onclick=\"javascript:handleRemove(" + result.id + ")\">Remove</a></td>";
+        let td6 = "<td>" + result.duration + "</td>";
+        let tr2 = "</tr></td>";
         elmTable.innerHTML = elmTable.innerHTML + tr + td1 + td2 + td3 + td4 + td5 + td6 + tr2;
     })
 }
 
 function handlePlay(i) {
     db.id3.where("id").equals(i).each(result => {
-        
-        gui.customSong(URL.createObjectURL(result.audio));
+        Nodes.playSongFromUrl(URL.createObjectURL(result.audio));
         //elmAudio.src = URL.createObjectURL(result.audio);
         elmImage0.src = result.img;
     })
@@ -149,9 +150,9 @@ function handleRemove(i) {
 
 function secondsToHms(d) {
 	d = Number(d);
-	var h = Math.floor(d / 3600);
-	var m = Math.floor(d % 3600 / 60);
-	var s = Math.floor(d % 3600 % 60);
+	let h = Math.floor(d / 3600);
+	let m = Math.floor(d % 3600 / 60);
+	let s = Math.floor(d % 3600 % 60);
 	return ((h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s);
 }
 
